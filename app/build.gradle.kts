58dcf6ad110043524c7ba6a5dbc285c4e9b5c774
Josemilk <voicemovil@gmail.com>
2026-09-05 22:30:00 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,8 +89,11 @@ android {
     isCoreLibraryDesugaringEnabled = true
   }
 
-  kotlinOptions {
-    jvmTarget = "17"
+  kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
+
   }
 
   buildFeatures {
